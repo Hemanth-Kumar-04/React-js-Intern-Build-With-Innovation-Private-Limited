7098f7405b755acbac8743ee79ebecaca0dc88fc
Hemanth-Kumar-04 <hemanthkumar.k.r04@gmail.com>
2023-12-30 20:11:12 +0530

    formatted

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = ({ setAuthToken }) => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('https://dummyjson.com/auth/login', {
-        method: 'POST',
+      const response = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           username: username,
@@ -22,7 +22,7 @@ const LoginForm = ({ setAuthToken }) => {
 
       if (!response.ok) {
         // Handle authentication failure, set error message
-        setError('Invalid credentials. Please try again.');
+        setError("Invalid credentials. Please try again.");
         return;
       }
 
@@ -32,22 +32,29 @@ const LoginForm = ({ setAuthToken }) => {
 
       // Store the authentication token in your app's state
       setAuthToken(authToken);
-
       // Navigate to home screen after successful login
-      navigate('/home');
+      navigate("/home");
     } catch (error) {
-      console.error('Error during login:', error);
+      console.error("Error during login:", error);
       // Handle other errors as needed
     }
   };
 
   return (
     <div>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
       <label>Username:</label>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
       <label>Password:</label>
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button onClick={handleLogin}>Login</button>
     </div>
   );
